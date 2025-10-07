a9abf2948f18eca12a1cb9cc1e0c442d1e9c35fd
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-10-07 16:30:55 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -5,10 +5,12 @@ import math
 GPIO.setmode(GPIO.BCM)
 
 rev = 14
+direction = 1
 GPIO.setup(rev, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def reverse(pin):
 	print("Rising edge detected on pin 14, reversing direction")
+	direction = -1 * direction
 
 GPIO.add_event_detect(rev, GPIO.RISING, callback=reverse, bouncetime=100)
 
@@ -31,10 +33,11 @@ try:
 		for pwm in led_pwm: # loops 
 			dc = (math.sin(2*math.pi*f*t-phi)**2)*100
 			pwm.ChangeDutyCycle(dc)
-			phi = phi + math.pi/11
+			phi = phi + direction * (math.pi/11)
 except KeyboardInterrupt:
 	print('\nExiting') 
 
 pwm.stop()
 GPIO.cleanup()
+
 
