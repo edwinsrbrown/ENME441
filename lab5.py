9b269f0ed144dfa6de99ac0e2019df7ab6c01cd3
edwinsrbrown <ebrown55@terpmail.umd.edu>
2025-10-07 16:10:10 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -10,20 +10,21 @@ GPIO_pins = list(range(2,12)) # list of all the GPIO pins
 led_pwm = [] # list to hold al of the PWMs
 
 for ledPin in GPIO_pins:
-	GPIO.setup(ledPin, GPIO.OUT) #set each GPIO pin as output
-	pwm = GPIO.PWM(ledPin, f_pwm) #create the PWM for each pin (with set f)
-	pwm.start(0) #start at 0% duty cycle
-	led_pwm.append(pwm) #put all PWM for each pin in a list
+	GPIO.setup(ledPin, GPIO.OUT) # set each GPIO pin as output
+	pwm = GPIO.PWM(ledPin, f_pwm) # create the PWM for each pin (with set f)
+	pwm.start(0) # start at 0% duty cycle
+	led_pwm.append(pwm) # put all PWM for each pin in a list
 
 
 try:
 	while 1:
-		t = time.time() #finds current time in seconds
-		phi = 0
-		for pwm in led_pwm:
+		t = time.time() # finds current time in seconds
+		phi = 0 # sets phi to 0 for Q1
+		for pwm in led_pwm: # loops 
 			dc = (math.sin(2*math.pi*f*t-phi)**2)*100
 			pwm.ChangeDutyCycle(dc)
 			phi = phi + math.pi/11
+			delay(0.5)
 except KeyboardInterrupt:
 	print('\nExiting') 
 
